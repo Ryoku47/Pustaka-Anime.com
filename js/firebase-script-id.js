6290d2264f7bfcf99d721f60bc81768747908f9c
Ryoku47 <57868430+Ryoku47@users.noreply.github.com>
2020-02-09 11:29:28 +0700

    Firebase fo postview counter

--- js/firebase-script-id.js
+++ js/firebase-script-id.js
@@ -1,24 +1,4 @@
-$.each($(&quot;a[name]&quot;), function(i, e) {
-var elem = $(e).parent().find(&quot;#postviews&quot;);
-var blogStats = new Firebase(&quot;https://pustaka-anime.firebaseio.com/pages/id/&quot; + $(e).attr(&quot;name&quot;));
-blogStats.once(&quot;value&quot;, function(snapshot) {
-var data = snapshot.val();
-var isnew = false;
-if(data == null) {
-data= {};
-data.value = 0;
-data.url = window.location.href;
-data.id = $(e).attr(&quot;name&quot;);
-isnew = true;
-}
-elem.text(data.value);
-data.value++;
-if(window.location.pathname!=&quot;/&quot;)
-{
-if(isnew)
-blogStats.set(data);
-else
-blogStats.child(&quot;value&quot;).set(data.value);
-}
-});
-});
+//<![CDATA[
+// View counter
+$(function(){var a=$(".viewers");a.each(function(){var n=$(this).attr("id"),e=new Firebase("https://pustaka-anime.firebaseio.com/pages/id/"+n);e.once("value",function(n){var i=n.val();null==i?i=1:"/"!=window.location.pathname&&i++,e.set(i),a.children("span").removeClass("loading").html(i)})})});
+//]]>
