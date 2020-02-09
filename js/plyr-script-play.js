edf156b02808b461784eb0bdf53386521f4388c8
Ryoku47 <57868430+Ryoku47@users.noreply.github.com>
2020-02-09 18:05:31 +0700

    Update Script Video Player Beta

--- js/plyr-script-play.js
+++ js/plyr-script-play.js
@@ -25,28 +25,5 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector(selector).addEventListener(type, callback, false);
   }
 
-  // Play
-  on('.js-play', 'click', () => { 
-    player.play();
-  });
-
-  // Pause
-  on('.js-pause', 'click', () => { 
-    player.pause();
-  });
-
-  // Stop
-  on('.js-stop', 'click', () => { 
-    player.stop();
-  });
-
-  // Rewind
-  on('.js-rewind', 'click', () => { 
-    player.rewind();
-  });
-
-  // Forward
-  on('.js-forward', 'click', () => { 
-    player.forward();
-  });
+  
 });
